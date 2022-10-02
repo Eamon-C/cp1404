26f1b1faaa164d9fc63cbc963df3d64685fc83d9
Eamon <ecour31@gmail.com>
2022-10-02 19:26:50 +1000

    files.py completed

--- prac_03/files.py
+++ prac_03/files.py
@@ -5,6 +5,27 @@ Several file-reading related codes.
 """
 
 # 1.
-name = open('name.txt')
+name = open('name.txt', 'w')
 name.write(input("What is your name? "))
 name.close()
+
+# 2.
+# in_file = open('name.txt', 'r')
+# print("Your name is", in_file.read())
+# in_file.close()
+
+# 3.
+# in_file = open('numbers.txt', 'r')
+# number_1 = int(in_file.readline())
+# number_2 = int(in_file.readline())
+# in_file.close()
+# print(number_1 + number_2)
+
+# 4.
+# total = 0
+# in_file = open('numbers.txt', 'r')
+# numbers = in_file.readlines()
+# in_file.close()
+# for line in numbers:
+#     total += int(line)
+# print(total)
