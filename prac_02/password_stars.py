77138cdcd2c4e4ec67546134c93d178d24b07755
Eamon <ecour31@gmail.com>
2022-10-03 17:54:08 +1000

    refactor password_stars.py to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,19 @@
-length_requirement = 6
-password = input("Password: ")
-while len(password) < length_requirement:
-    print("Insufficient length.")
+def main():
+    length_requirement = 6
+    password = get_password(length_requirement)
+    print_stars(password)
+
+
+def print_stars(password):
+    print("*" * len(password))
+
+
+def get_password(length_requirement):
     password = input("Password: ")
-print("*" * len(password))
+    while len(password) < length_requirement:
+        print("Insufficient length.")
+        password = input("Password: ")
+    return password
+
+
+main()
