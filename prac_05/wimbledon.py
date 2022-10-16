f3f68f9bea0de18b3a0f89e25e696194965810a5
Eamon <ecour31@gmail.com>
2022-10-16 19:27:35 +1000

    finish wimbledon.py

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,47 @@
+"""
+CP1404 prac
+wimbledon.py
+Estimate: 45min
+Actual: 1hour
+"""
+
+FILENAME = "wimbledon.csv"
+
+
+def main():
+    data = (get_data())
+    champion_to_count, countries = process_data(data)
+    display_results(champion_to_count, countries)
+
+
+def display_results(champion_to_count, countries):
+    print("Wimbledon Champions: ")
+    for name, count in champion_to_count.items():
+        print(name, count)
+    print(f"\nThese {len(countries)} countries have won Wimbledon: ")
+    print(", ".join(country for country in sorted(countries)))
+
+
+def process_data(data):
+    champion_to_count = {}
+    countries = set()
+    for line in data:
+        countries.add(line[1])
+        try:
+            champion_to_count[line[2]] += 1
+        except KeyError:
+            champion_to_count[line[2]] = 1
+    return champion_to_count, countries
+
+
+def get_data():
+    data = []
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split(",")
+            data.append(parts)
+    return data
+
+
+main()
