0d082ff979f68ae6c2136f8fe5b6c633ae20c246
Eamon <ecour31@gmail.com>
2022-10-19 20:14:28 +1000

    fixed hex_colours.py to display names better.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -7,7 +7,7 @@ name_to_code = {'Absolute Zero': '#0048ba', 'Acid Green': '#b0bf1a', 'Alizarin c
                 'Black': '#000000', 'Canary': '#ffff99', 'Cyclamen': '#f56fa1', 'Daffodil': '#ffff31',
                 'DarkKhaki': '#bdb76b', 'Glossy Grape': '#ab92b3'}
 
-max_len = len(max(name for name in name_to_code))
+max_len = max(len(name) for name in name_to_code)
 
 for colour_name, colour_code in name_to_code.items():
     print(f"{colour_name:>{max_len}} is {colour_code}")
