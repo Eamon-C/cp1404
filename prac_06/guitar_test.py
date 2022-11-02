2fb1ad73c90d28568523cddfbaf5e19c9d0e2b87
Eamon <ecour31@gmail.com>
2022-11-02 13:44:51 +1000

    complete guitar_test.py and add guitars.py

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,17 @@
+"""
+CP1404 prac, week 6
+guitar_test.py
+"""
+
+from prac_06.guitar import Guitar
+
+
+def main():
+    guitar = Guitar("Gibson :-5 CES", 1922, 16035.40)
+    guitar_2 = Guitar("Another guitar", 2013, 100)
+    print(guitar, guitar_2)
+    print(guitar.get_age(), guitar_2.get_age())
+    print(guitar.is_vintage(), guitar_2.is_vintage())
+
+
+main()
