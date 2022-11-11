cdccefc920a5eaad3265e182ab0b48cb36acda5e
Eamon <ecour31@gmail.com>
2022-11-11 17:39:41 +1000

    attempt date sorting for project_management.py

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -2,7 +2,7 @@
 CP1404 prac, week 7
 project_management.py
 estimated time: 3 hours
-actual: 12:00
+actual: 4 hours
 """
 
 import datetime
@@ -28,22 +28,33 @@ def main():
             extract_data(filename, projects)
             projects.sort()
             display_projects(projects)
-        # elif choice == "F":
-        #
+        elif choice == "F":
+            filer_projects(projects)
         # elif choice == "A":
         #
         # elif choice == "U":
-        break
+        choice = input(MENU).upper()
     print("Thank you for using custom-built project management software.")
+
+
+def filer_projects(projects):
+    """filters projects based on date. !! THIS DOES NOT WORK !!"""
+    date_string = input("Show projects that start after date (dd/mm/yyyy): ")
+    date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
+    for project in projects:
+        if datetime.datetime.strptime(project[1].start_date, "%d/%m/%Y") >= date:
+            print(project)
 
 
 def display_projects(projects):
     """displays projects separated by completion status and ordered by priority"""
+
     print("Incomplete projects:")
     for project in projects:
         if not project.is_complete():
             print(f"    {project.name}, start: {project.start_date}, priority {project.priority}, estimate: "
                   f"${project.cost_estimate:.2f}, completion: {project.completion_percentage}%")
+
     print("Complete projects:")
     for project in projects:
         if project.is_complete():
